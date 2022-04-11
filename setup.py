05fd82c9625b48d83466daf699fb0fc1f4528296
andenrx <andenrx@gmail.com>
2022-04-11 10:24:43 -0700

    Initial Code

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='bookmarked-utils',
+    version='0.1',
+    description='bmark',
+    url='https://github.com/EEzycade/font-software-data',
+    packages=find_packages(),
+    install_requires=[],
+)
